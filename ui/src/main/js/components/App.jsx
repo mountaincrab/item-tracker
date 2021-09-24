2a7adc218f8ce3c9b445933c5bda09ef69861a96
Chris Crabtree <chris.crabtree@matillion.com>
2021-09-24 13:06:19 +0100

    Prepare for material UI

--- ui/src/main/js/components/App.jsx
+++ ui/src/main/js/components/App.jsx
@@ -1,23 +1,8 @@
 import React, {useEffect, useState} from "react";
 import CreateContainerDialog from "./CreateContainerDialog";
 import ContainerList from "./ContainerList";
-import containerService from "../services/ContainerService";
 
-let p = new Promise((resolve, reject) => {
-  setTimeout(() => resolve(10), 1000);
-})
-.then(
-    resolved => {
-      console.log("Promise 1 resolved to " + resolved);
-      return resolved + 10;
-    },
-    rejected => {
-      console.log("Promise 1 rejected to " + rejected);
-      return rejected;
-});
-// .then(answer => new Promise(resolve, reject) => {
-//   setTimeout(() => resolve(answer))
-// });
+import containerService from "../services/ContainerService";
 
 
 function App() {
@@ -87,6 +72,7 @@ function App() {
 
   return (
       <div>
+        <Button>test</Button>
         <button onClick={resetContainers}>Reset</button>
         <CreateContainerDialog createContainer={createContainer}/>
         <ContainerList
