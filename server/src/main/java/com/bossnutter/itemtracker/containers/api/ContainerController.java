0831fe53a254372076e7a30e9f83c5214cd05abd
Chris Crabtree <chris.crabtree@matillion.com>
2023-03-31 07:25:51 +0100

    Remove keycloak

--- server/src/main/java/com/bossnutter/itemtracker/containers/api/ContainerController.java
+++ server/src/main/java/com/bossnutter/itemtracker/containers/api/ContainerController.java
@@ -4,7 +4,6 @@ import com.bossnutter.itemtracker.containers.ContainerMapper;
 import com.bossnutter.itemtracker.containers.api.response.ContainerResponse;
 import com.bossnutter.itemtracker.containers.repository.ContainerRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,15 +26,6 @@ public class ContainerController {
 
     @GetMapping("/containers")
     public List<ContainerResponse> getContainers() {
-        return containerRepository.findAll()
-                .stream()
-                .map(containerMapper::domainToResponse)
-                .toList();
-    }
-
-    @CrossOrigin(origins = "*")
-    @GetMapping("/containers-new")
-    public List<ContainerResponse> getContainersNew() {
         return containerRepository.findAll()
                 .stream()
                 .map(containerMapper::domainToResponse)
